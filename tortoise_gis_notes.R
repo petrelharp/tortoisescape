a11f175751734d1546e3a1d90cebf5e9017f7083
Gideon Bradburd <gideon.bradburd@gmail.com>
2014-08-27 18:38:24 -0700

    added road layer redux Robj

--- tortoise_gis_notes.R
+++ tortoise_gis_notes.R
@@ -15,7 +15,7 @@ require(raster)
 # create a raster of one of the .tifs.  this creates a link
 #	between R and the specific file, from which it will dynamically
 #	pull info required of it.
-road <- raster("road_30.tif")
+road <- raster("~/Downloads/tortTestGIS/road_30.tif")
 
 # check out what a raster is all about
 str(road)
@@ -39,9 +39,27 @@ rm(road) ; gc()
 road.redux <- raster("road_redux.grd")
 	plot(road.redux)
 
+
+# get x,y coordinates for each cell
+row.coords <- yFromRow(road.redux)
+col.coords <- xFromCol(road.redux)
+
 # now we read the raster into working memory as a matrix, 
 #	dropping metadata along the way, and have a standard
 #	R matrix object that we can do with as we wish
-road.redux.mat <- as.matrix(road.redux)
+road.redux.matrix <- as.matrix(road.redux)
 
-	
+# then we can add x and y coordinates to the matrix
+rownames(road.redux.matrix) <- row.coords
+colnames(road.redux.matrix) <- col.coords
+
+# and we can save that object to work with
+save(file="road_redux_matrix.Robj",road.redux.matrix)
+
+if(FALSE){
+	# now, try to read in a geographic coordinate system
+	lat <- raster("~/Downloads/evan_dt_gis/lat_30_utm_tif/lat_gcs_30.tif")
+		aggregate(lat,fact=10,fun=mean,na.rm=TRUE,filename="lat_redux")
+	long <- raster("~/Downloads/evan_dt_gis/lat_30_utm_tif/lon_gcs_30.tif")
+		aggregate(long,fact=10,fun=mean,na.rm=TRUE,filename="long_redux")
+}
