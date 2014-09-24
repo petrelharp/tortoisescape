010648f9b462ea15a711293bc35c86c3af1ee58a
peter <petrel.harp@gmail.com>
2014-09-24 12:11:48 -0700

    setup G amended to work with lists of layers

--- inference/setup-real-G.R
+++ inference/setup-real-G.R
@@ -3,42 +3,42 @@
 source("resistance-fns.R")
 require(raster)
 
-if (!interactive()) { layer.prefix <- commandArgs(TRUE)[1] }
+if (!interactive()) { 
+    layer.prefix <- commandArgs(TRUE)[1] 
+    layer.file <- commandArgs(TRUE)[2]
+} else {
+    layer.file <- "../inference/six-raster-list"
+    layer.prefix <- c("../geolayers/TIFF/100x/crop_resampled_masked_aggregated_100x_")
+}
 
 # for (layer.prefix in c( "../geolayers/TIFF/100x/crop_resampled_masked_aggregated_100x_", "../geolayers/TIFF/10x/crop_resampled_masked_aggregated_10x_", "../geolayers/TIFF/masked/crop_resampled_masked_" ) ) {
 
-    # minimal list to make sure this works with update.G below
-    layer.names <- c("annual_precip","lon_gcs_30")
+    # # minimal list to make sure this works with update.G below
+    # layer.names <- c("annual_precip","lon_gcs_30")
+    layer.names <- paste( basename(layer.file), "-na", sep='' )  # this is TRUE in cells with missing data
 
     init.params <- c( beta=1.0, gamma=rep(.01,length(layer.names)), delta=rep(.01,length(layer.names)) )
 
     ###
     # layer whatnot
 
-    # get info out of one layer
-    onelayer <- raster(paste(layer.prefix,layer.names[1],sep=''))
-    n <- dim(onelayer)[2]; m <- dim(onelayer)[1]
+    # get info out of na layer
+    nalayer <- raster(paste(dirname(layer.prefix),"/",layer.names[1],sep=''))
+    n <- dim(nalayer)[2]; m <- dim(nalayer)[1]
     nmap <- matrix(1:n*m,nrow=n)
     all.locs <- cbind( i=as.vector(row(nmap)), j=as.vector(col(nmap)) )
-    onevals <- values(onelayer)
-    nonmissing <- which(!is.na(onevals))
+    nonmissing <- which(values(nalayer)==0)
 
     ###
     # generator matrix
-    ij <- adjacent(onelayer,cells=nonmissing,target=nonmissing,directions=4,pairs=TRUE,sorted=TRUE) # to and from cells both loc
+    ij <- adjacent(nalayer,cells=nonmissing,target=nonmissing,directions=4,pairs=TRUE,sorted=TRUE) # to and from cells both loc
     ij <- ij[,2:1]
     stopifnot( all(ij[,1] != ij[,2]) ) ## NO DIAGONAL
 
     G <- sparseMatrix( i=match(ij[,1],nonmissing), j=match(ij[,2],nonmissing), x=1.0 )
     Gjj <- rep( seq.int(length(G@p)-1), diff(G@p) )
 
-    layers <- sapply(layer.names, function (ll) {
-                rast <- raster(paste(layer.prefix,ll,sep=''))
-                # note this is ROW-ORDERED
-                # so to plot do:  dim(x) <- dim(rast)[2:1]; image(x)
-                vrast <- scale( values(rast)[nonmissing] )
-                return(vrast)
-            } )
+    layers <- cbind( values(nalayer)[nonmissing] ) # should be all zero, but heck
     stopifnot(nrow(layers)==nrow(G))
 
     transfn <- exp
