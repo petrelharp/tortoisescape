2a8624bed2f431a32eb84339ba9574d915f31602
peter <petrel.harp@gmail.com>
2014-09-26 16:45:24 -0700

    .

---
+++ geodist/get-all-edists-10x.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+for x in $(cat ../inference/twentyfour-raster-list)
+do
+    qsub -vARGS="'""get-environmental-distance.R ../geolayers/TIFF/10x/crop_resampled_masked_aggregated_10x_ 10x $x""'" ../cluster/single-skeleton.pbs
+done
+
+exit
+
