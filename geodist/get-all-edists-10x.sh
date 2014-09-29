f4ef9faf62559d3714644c14a3d140d3a9dffb2d
peter <petrel.harp@gmail.com>
2014-09-29 09:33:49 -0700

    pbs

--- geodist/get-all-edists-10x.sh
+++ geodist/get-all-edists-10x.sh
@@ -3,7 +3,8 @@
 
 for x in $(cat ../inference/twentyfour-raster-list)
 do
-    qsub -vARGS="'""get-environmental-distance.R ../geolayers/TIFF/10x/crop_resampled_masked_aggregated_10x_ 10x $x""'" ../cluster/single-skeleton.pbs
+    qsub -vARGS="'""get-environmental-distance.R ../geolayers/TIFF/10x/crop_resampled_masked_aggregated_10x_ 10x $x mean""'" ../cluster/single-skeleton.pbs
+    qsub -vARGS="'""get-environmental-distance.R ../geolayers/TIFF/10x/crop_resampled_masked_aggregated_10x_ 10x $x max""'" ../cluster/single-skeleton-dl165.pbs
 done
 
 exit
