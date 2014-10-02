709273f53b6f52362a7786877b61702bf2da4407
peter <petrel.harp@gmail.com>
2014-10-02 09:51:20 -0700

    added lowess lines to quantiles of ibd-ibe plot and colored by rank rather than value

--- tort_report_figures/tort_ibde_fig.R
+++ tort_report_figures/tort_ibde_fig.R
@@ -8,16 +8,16 @@
 #	some preliminaries
 ################################
 
-if(file.exists("~/Desktop/Dropbox/tortoisescape")){
-	setwd("~/Desktop/Dropbox/tortoisescape")
+if(file.exists("~/Desktop/Dropbox/tortoisescape/tort_report_figures")){
+	setwd("~/Desktop/Dropbox/tortoisescape/tort_report_figures")
 }
 
 # get the metadata
-torts <- read.csv("1st_180_torts.csv",header=TRUE,stringsAsFactors=FALSE)
+torts <- read.csv("../1st_180_torts.csv",header=TRUE,stringsAsFactors=FALSE)
 nind <- nrow(torts)
 
 # and the pairwise geographic distance matrix
-tort.dist.table <- read.table("1st180_pairwise_distances_sorted_redundancy_removed.txt",header=TRUE)
+tort.dist.table <- read.table("../1st180_pairwise_distances_sorted_redundancy_removed.txt",header=TRUE)
 tort.dists <- numeric(nind^2); dim(tort.dists) <- c(nind,nind)
 tort.dists[ cbind( match(tort.dist.table$etort1,torts$EM_Tort_ID), 
 					match(tort.dist.table$etort2,torts$EM_Tort_ID) ) ] <- tort.dist.table$DISTANCE
@@ -25,7 +25,7 @@ tort.dists <- tort.dists + t(tort.dists)
 
 
 # then grab the pairwise pi matrix
-tort.pwp.vals <- scan("pairwisePi/alleleCounts_1millionloci.pwp")
+tort.pwp.vals <- scan("../pairwisePi/alleleCounts_1millionloci.pwp")
 tort.pwp <- numeric(nind^2)
 dim(tort.pwp) <- c(nind,nind)
 	tort.pwp[upper.tri(tort.pwp,diag=TRUE)] <- tort.pwp.vals
@@ -56,7 +56,7 @@ matrixify.edist.tsv <- function(tsv.file,nind,EM_Tort_ID){
 
 make.edist.plotting.colors <- function(edist.mat,index.mat){
 	n.cols <- length(edist.mat[index.mat])
-	edist.plotting.cols <- heat.colors(n.cols)[as.numeric(cut(edist.mat[index.mat],n.cols))]
+	edist.plotting.cols <- adjustcolor(diverge_hcl(n.cols,c=100),.5)[as.numeric(cut(order(edist.mat[index.mat]),n.cols))]   # [as.numeric(cut(edist.mat[index.mat],n.cols))]
 	return(edist.plotting.cols)
 }
 
@@ -66,28 +66,40 @@ make.edist.plotting.colors <- function(edist.mat,index.mat){
 #	and IBD&E .png
 ################################
 
-if(file.exists("~/Desktop/10x")){
-	setwd("~/Desktop/10x")
+if (file.exists("~/Desktop/10x")){
+	tsvdir <- "~/Desktop/10x"
+} else {
+    tsvdir <- "../geodist/10x"
 }
-index.mat <- upper.tri(tort.pwp,diag=TRUE)
-tsv.files <- list.files(pattern="*.tsv")
+
+outdir <- "ibde.pngs/"
+
+index.mat <- upper.tri(tort.pwp,diag=FALSE)
+tsv.files <- list.files(tsvdir,pattern="*.tsv",full.names=TRUE)
 tsv.file.names <- gsub(".tsv","",tsv.files)
 
-tort.pwp <- tort.pwp[index.mat]
-tort.dists <- tort.dists[index.mat]
+tort.pwp.ut <- tort.pwp[index.mat]
+tort.dists.ut <- tort.dists[index.mat]
 
-if(!file.exists("ibde.pngs")){
-	dir.create("ibde.pngs")
+if(!file.exists(outdir)){
+	dir.create(outdir)
 }
 for(i in 1:length(tsv.files)){
 	cat(i,"\t")
 	edist.mat <- matrixify.edist.tsv(tsv.files[i],nind,torts$EM_Tort_ID)
 	edist.plotting.cols <- make.edist.plotting.colors(edist.mat,index.mat)
-	png(file=paste("ibde.pngs/",tsv.file.names[i],"ibde.png",sep=""),res=200,width=8*200,height=5*200)
-		plot(tort.dists,tort.pwp,col=edist.plotting.cols,pch=20,cex=0.6,
+	png(file=paste(outdir,basename(tsv.file.names[i]),"ibde.png",sep=""),res=200,width=8*200,height=5*200)
+		plot(tort.dists.ut,tort.pwp.ut,col=edist.plotting.cols,pch=20,cex=0.5,
 				xlab="pairwise geo distance",
 				ylab="pairwise pi",
-				main=paste("IBD&E: ",tsv.file.names[i],sep=""))
+				main=paste("IBD&E: ",basename(tsv.file.names[i]),sep=""))
+        edist.qs <- quantile(edist.mat[index.mat],c(0,.25,.5,.75,1),na.rm=TRUE)
+        qcols <- rev(diverge_hcl(4,c=100))
+        for (k in 1:4) {
+            usethese <- (!is.na(edist.mat[index.mat])) & ( edist.mat[index.mat] >= edist.qs[k] ) & ( edist.mat[index.mat] < edist.qs[k+1] )
+            if (any(usethese)) { lines( lowess( tort.dists.ut[usethese], tort.pwp.ut[usethese], f=.2 ), col=qcols[k], lwd=2 ) }
+        }
+        legend("bottomright", lwd=2, col=qcols, legend=paste(c("first","second","third","fourth"),"quartile"), title=basename(tsv.file.names[i]) )
 	dev.off()
 }
 
