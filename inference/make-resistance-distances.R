fe0e4a578b836e7f85187534f1a038009adbded7
peter <petrel.harp@gmail.com>
2014-10-03 23:42:26 -0700

    .

--- inference/make-resistance-distances.R
+++ inference/make-resistance-distances.R
@@ -48,9 +48,14 @@ layer.names <- scan(layer.file,what="char")
 load( paste(subdir,"/",basename(layer.prefix),"_",basename(layer.file),"_","G.RData",sep='') ) # provides "G"        "update.G" "ndelta"   "ngamma"   "transfn"  "valfn"    "layers"
 Gjj <- rep( seq.int(length(G@p)-1), diff(G@p) )
 
+load( paste( subdir, "/", basename(layer.prefix), "_", basename(layer.file), "_neighborhoods.RData", sep='' ) ) # provides 'neighborhoods'
 load(paste(subdir,"/",basename(layer.prefix),"tortlocs.RData",sep='')) # provides 'locs'
+
+# REMOVE MISSING INDIV
 na.indiv <- which( is.na( locs ) )
 locs <- locs[-na.indiv]
+neighborhoods <- neighborhoods[-na.indiv]
+
 
 ##
 # initial parameters?
