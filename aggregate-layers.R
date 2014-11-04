a70b2e346e52cc59e6f678a65473b24fffc7d82a
peter <petrel.harp@gmail.com>
2014-11-03 18:55:59 -0800

    script to create new resolutions

---
+++ aggregate-layers.R
@@ -0,0 +1,38 @@
+#!/usr/bin/Rscript
+
+# Create a set of new layers with a different resolution
+
+if (!interactive) {
+    agfact <- as.numeric( commandArgs(TRUE)[1] )
+    outdir <- commandArgs(TRUE)[2]
+    infiles <- commandArgs(TRUE)[-(1:2)]
+} else {
+    agfact <- 2.5
+    outdir <- "geolayers/TIFF/250x"
+    infiles <- list.files("geolayers/TIFF/100x",pattern="*.(tif)|(grd)",full.names=TRUE)
+}
+
+require(parallel)
+numcores <- as.numeric(scan(pipe("cat /proc/cpuinfo | grep processor | tail -n 1 | awk '{print $3}'")))+1
+
+require(raster)
+rasterOptions(tmpdir=".")
+
+dir.create(outdir,recursive=TRUE)
+
+mclapply( infiles, function (infile) {
+        tryCatch({
+                cat("Beginning on ", infile, " .\n")
+                raster2crop <- raster(infile)
+                aggregate(raster2crop,
+                            fact=agfact,
+                            fun=mean,
+                            na.rm=TRUE,
+                            filename=paste(outdir,gsub("(tif)|(grd)$","",basename(infile)),sep=""),
+                            overwrite=TRUE)
+			 },error=function(e){cat("problem with",infile,"\n")})
+        removeTmpFiles()
+        cat("Done with ", infile, " .\n")
+    }, mc.cores=numcores )
+
+cat("All done.\n")
