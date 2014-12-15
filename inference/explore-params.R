398fa8eed73eb1ab1aef1cbc304996fa45895400
peter <petrel.harp@gmail.com>
2014-12-15 15:33:23 -0800

    .

--- inference/explore-params.R
+++ inference/explore-params.R
@@ -2,7 +2,6 @@
 layer.prefix <- "../geolayers/multigrid/256x/crm_"
 subdir <- "256x"
 layer.file <- "dem-layer-list"
-param.file <- "params-dem-layer-list.tsv"
 
 
 source("resistance-fns.R")
@@ -25,41 +24,40 @@ locs <- locs[setdiff(seq_along(locs),na.indiv)]
 neighborhoods <- lapply(neighborhoods[setdiff(seq_along(locs),na.indiv)],function (x) { x[!is.na(x)] })
 
 
-##
-# initial parameters?
-#   in time t, 1D RW with rate r does t*r jumps, displacement has variance t*r
-#   so time to move N grid sites away is sqrt(N)/r
-#   so if hitting times are of order T, want r of order sqrt(N)/T
-
-init.param.table <- read.table( param.file, header=TRUE )
-init.params <- unlist( init.param.table[ match( subdir, init.param.table[,1] ), -1 ] )
-
+## END SETUP
 
 ph <- plot.ht.fn(layer.prefix,"dem_30_m800_sq",nonmissing)
-dem <- raster(paste(layer.prefix,"dem_30_m800_sq",sep=''))
+ph( layers[,1] )
 
 dothese <- c(1,10,19,83)
 
-newparams <- function (dothese) {
+newparams <- function (params,dothese) {
+    # params are parameters: for n layers,
+    #  params[1] is beta, overall multiplicative constant
+    #  params[2:(n+1)] is gamma, weights on the layers that give the stationary distribution
+    #  params[(n+2):(2*n+1)] is delta, the weights on the layers that give the jump rates
+    # dothese is a vector of indices of tortoise locations to compute hitting times of
     G@x <- update.G(params)
     hts <- hitting.analytic( neighborhoods[dothese], G-diag(rowSums(G)), numcores=numcores )
     hts[hts<0] <- NA
-    ymax <- 1.5*max(hts[locs,])
-    for (k in seq_along(dothese)) { ph(pmin(ymax,hts[,k])); } # ph(pmin(6,log10(hts[,k])))  }
-    plot( hts[locs,], pimat[,dothese], col=col(pimat[,dothese]) )
+    # 'locs' are indices of tortoise locations
+    ymax <- 1.5*max(hts[locs,])  # 1.5 times maximum hitting time to another tortoise location
+    for (k in seq_along(dothese)) { ph(pmin(ymax,hts[,k])); } # plot with maximum value at ymax
+    plot( hts[locs,], pimat[,dothese], col=col(pimat[,dothese]), xlab='hitting time', ylab='divergence' )
     abline(0,1)
     invisible(hts)
 }
 
 layout(matrix(1:6,nrow=2,byrow=TRUE))
 
-params <- c(1e-4,-2,-9); hts <- newparams(c(10,83))
-params <- c(.01,0,-1); hts <- newparams(c(10,83))
+hts <- newparams(c(1e-4,-2,-9),c(10,83))
+hts <- newparams(c(.01,0,-1),c(10,83))
 
-params <- c(.01,0,-3); hts <- newparams(c(10,83))
-params <- c(.01,-.1,-3); hts <- newparams(c(10,83))
+hts <- newparams(c(.01,0,-3),c(10,83))
+hts <- newparams(c(.01,-.1,-3),c(10,83))
 
 
 # what does a transformed layer look like?
+dem <- raster(paste(layer.prefix,"dem_30_m800_sq",sep=''))
 plot( exp((-2)*dem) )
 
