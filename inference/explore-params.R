128366a97739c40272129ffc6e0d57dd41604cfb
peter <petrel.harp@gmail.com>
2014-12-15 14:50:10 -0800

    added 256x layers

--- inference/explore-params.R
+++ inference/explore-params.R
@@ -21,8 +21,8 @@ load(paste(subdir,"/",basename(layer.prefix),basename(layer.file),"-","setup.RDa
 
 # REMOVE MISSING INDIV
 na.indiv <- which( is.na( locs ) )
-locs <- locs[-na.indiv]
-neighborhoods <- lapply(neighborhoods[-na.indiv],function (x) { x[!is.na(x)] })
+locs <- locs[setdiff(seq_along(locs),na.indiv)]
+neighborhoods <- lapply(neighborhoods[setdiff(seq_along(locs),na.indiv)],function (x) { x[!is.na(x)] })
 
 
 ##
@@ -46,37 +46,20 @@ newparams <- function (dothese) {
     hts[hts<0] <- NA
     ymax <- 1.5*max(hts[locs,])
     for (k in seq_along(dothese)) { ph(pmin(ymax,hts[,k])); } # ph(pmin(6,log10(hts[,k])))  }
+    plot( hts[locs,], pimat[,dothese], col=col(pimat[,dothese]) )
+    abline(0,1)
     invisible(hts)
 }
 
-layout(matrix(1:4,nrow=2))
+layout(matrix(1:6,nrow=2,byrow=TRUE))
+
 params <- c(1e-4,-2,-9); hts <- newparams(c(10,83))
 params <- c(.01,0,-1); hts <- newparams(c(10,83))
+
 params <- c(.01,0,-3); hts <- newparams(c(10,83))
 params <- c(.01,-.1,-3); hts <- newparams(c(10,83))
 
+
+# what does a transformed layer look like?
 plot( exp((-2)*dem) )
 
-# for talk
-params <- c(.01,0,-1); hts1<- newparams(c(10,83))
-params <- c(.01,0,-3); hts2 <- newparams(c(10,83))
-
-ph <- plot.ht.fn(layer.prefix,"dem_30_m800_sq",nonmissing,par.args=list(mai=c(.7,.7,.1,.15),mar=c(3,3,0,0)+.1))
-
-# see raster:::.rasterImagePlot
-
-pdf(file="../../talks/images/example-hts.pdf", width=5, height=3.5, pointsize=10)
-layout(matrix(1:4,nrow=2,byrow=TRUE))
-for (k in 1:2) {
-    ph(pmin(ymax,hts[,k]),legend.mar=0,legend=FALSE)
-    ph(pmin(ymax,hts[,k]),legend.mar=0,legend=FALSE)
-}
-dev.off()
-
-
-###
-# find parameters that best fit the data?
-
-
-
-hts <- hitting.analytic( neighborhoods, G-diag(rowSums(G)), numcores=numcores )
