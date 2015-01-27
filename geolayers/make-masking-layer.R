be99bcd4a63218e3e70bb8226ce9351a4c001fde
peter <petrel.harp@gmail.com>
2015-01-26 22:32:15 -0800

    .

--- geolayers/make-masking-layer.R
+++ geolayers/make-masking-layer.R
@@ -2,18 +2,18 @@
 
 usage <- "Combine crew, dem_2K_mask, and sea_NA_30 into a single layer for masking.
 Usage:
-    Rscript make-masking-layer.R (name of directory) 
+    Rscript make-masking-layer.R (name of directory) (aggregation factor for clumping)
 "
 
 argvec <- if (interactive()) { scan(what='char') } else { commandArgs(TRUE) }
-if ( length(argvec)<1 ) { stop(usage) }
+if ( length(argvec)<2 ) { stop(usage) }
 
 require(raster)
 rasterOptions(tmpdir=".")
 
-orig.dir <- getwd()
-
 subdir <- argvec[1]
+ag.fact <- argvec[2]
+
 setwd(subdir)
 grd.or.tif <- function (x) { paste(x, if (file.exists(paste(x,".grd",sep=''))) { ".grd" } else { ".tif" }, sep='' ) }
 crew <- raster(grd.or.tif("crew_30_NA"))
@@ -25,11 +25,12 @@ masked <- mask(masked,sea)
 d.mask <- distance(masked)
 mask.exp <- ( d.mask > 300 )
 mask.exp[mask.exp==0] <- NA
-mask.ag <- aggregate(mask.exp,fact=64,fun=mean,na.rm=FALSE)
+mask.ag <- aggregate(mask.exp,fact=ag.fact,fun=mean,na.rm=FALSE)
 mask.clumps <- clump(mask.ag)
 clump.sizes <- table(values(mask.clumps))
 big.clump <- which.max( clump.sizes )
 mask.ag[mask.clumps!=big.clump] <- NA
-mask.disag <- disaggregate( mask.ag, fact=64 )
+mask.disag <- disaggregate( mask.ag, fact=ag.fact )
 masked <- mask( masked, mask.disag )
 writeRaster(masked,file="mask_crew_dem_2K_sea.grd")
+removeTmpFiles()
