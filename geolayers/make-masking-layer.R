4c460cde5a989387615d1066437186ac589e079d
peter <petrel.harp@gmail.com>
2015-01-26 22:56:20 -0800

    .

--- geolayers/make-masking-layer.R
+++ geolayers/make-masking-layer.R
@@ -9,7 +9,6 @@ argvec <- if (interactive()) { scan(what='char') } else { commandArgs(TRUE) }
 if ( length(argvec)<2 ) { stop(usage) }
 
 require(raster)
-rasterOptions(tmpdir=".")
 
 subdir <- argvec[1]
 ag.fact <- as.numeric(argvec[2])
@@ -18,11 +17,13 @@ setwd(subdir)
 grd.or.tif <- function (x) { paste(x, if (file.exists(paste(x,".grd",sep=''))) { ".grd" } else { ".tif" }, sep='' ) }
 crew <- raster(grd.or.tif("crew_30_NA"))
 dem.mask <- raster(grd.or.tif("dem_2K_mask"))
-water <- raster(grd.or.tif("water0_30"))
 sea <- raster(grd.or.tif("sea_NA_30"))
 masked <- mask(crew,dem.mask)
 masked <- mask(masked,sea)
-d.mask <- distance(masked)
+inv.masked <- masked
+inv.masked[is.na(masked)] <- 1
+inv.masked[!is.na(masked)] <- NA
+d.mask <- distance(inv.masked)
 mask.exp <- ( d.mask > 300 )
 mask.exp[mask.exp==0] <- NA
 mask.ag <- if (ag.fact>1) { aggregate(mask.exp,fact=ag.fact,fun=mean,na.rm=FALSE) } else { mask.exp }
