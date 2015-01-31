ffcb22ec3e7949b52e382c91322d90b2e34109b3
peter <petrel.harp@gmail.com>
2015-01-30 22:21:04 -0800

    .

--- inference/summarize-runs.sh
+++ inference/summarize-runs.sh
@@ -5,4 +5,12 @@ then
     source /home/rcf-40/pralph/cmb/bin/R-setup-usc.sh
 fi
 
-Rscript -e 'options(width=1000);xx <- t(sapply(commandArgs(TRUE), function (x) { load(x); y <- trust.optim[c("value","iterations","converged")]; y[sapply(y,is.null)]<-NA; unlist(y) } )); xx[order(xx[,1]),]' $(find "$@" -name 'inference-*.RData')
+Rscript <(echo 'options(width=1000);
+    xx <- t(sapply(commandArgs(TRUE), function (x) { 
+        load(x); 
+        y <- trust.optim[c("value","iterations","converged")];
+        y[sapply(y,is.null)]<-NA;
+        y[[1]] <- sqrt(y[[1]])/(nrow(trust.optim$pimat)*(ncol(trust.optim$pimat)-1))
+        unlist(y) } )); 
+    xx[order(xx[,1]),]') \
+    $(find "$@" -name 'inference-*.RData')
