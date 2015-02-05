4228fe4c846f89d65db168aee13bdc145a0e4531
peter <petrel.harp@gmail.com>
2015-02-05 15:08:53 -0800

    .

--- inference/summaries/summary-to-html.R
+++ inference/summaries/summary-to-html.R
@@ -2,20 +2,23 @@
 
 x <- read.csv("all-collated.csv",stringsAsFactors=FALSE)
 x$file <- file.path("..",x$file)
-x$fit.all.png <- gsub("inference-","comparison-",gsub(".RData","-all.png",x$file))
-x$fit.habitat.png <- gsub("inference-","comparison-",gsub(".RData","-habitat-only.png",x$file))
-x$eval.all <- file.path(dirname(x$file),"evaluate-alt_pref_pda-all.html")
-x$eval.habitat <- file.path(dirname(x$file),"evaluate-alt_pref_pda-habitat-only.html")
+x$fit.png <- paste(gsub("inference-","comparison-",gsub(".RData","-",x$file)),x$summary,".png",sep="")
+x$fit.html <- gsub(".RData",".html",x$file)
+x$alt_pref <- file.path(dirname(x$file),paste("evaluate-alt_pref_pda-",x$summary,".html",sep=''))
+x$alt_1 <- file.path(dirname(x$file),paste("evaluate-alt_1_pda-",x$summary,".html",sep=''))
+x$alt_2 <- file.path(dirname(x$file),paste("evaluate-alt_2_pda-",x$summary,".html",sep=''))
+x$alt_3 <- file.path(dirname(x$file),paste("evaluate-alt_3_pda-",x$summary,".html",sep=''))
+x$alt_4 <- file.path(dirname(x$file),paste("evaluate-alt_4_pda-",x$summary,".html",sep=''))
 
 print.vars <- c("summary","mad","mse","converged","n.refs","file")
-xt <- ( cbind( x[,print.vars], 
-                fit.all.png="none",
-                fit.habitat.png="none",
-                eval.all="none",
-                eval.habitat="none"
-            , stringsAsFactors=FALSE) )
-for (xn in c("fit.all.png","fit.habitat.png","eval.all","eval.habitat")) {
+xt <- x[,print.vars]
+for (xn in c("fit.png","fit.html","alt_pref","alt_1","alt_2","alt_3","alt_4")) {
+    xt[xn] <- "none"
     xt[file.exists(x[,xn]),xn] <- paste("<a href='",x[file.exists(x[,xn]),xn],"'>link</a>",sep='')
+}
+null.ind <- match("null",x$summary)
+for (xn in c("summary","fit.png")) {
+    xt[null.ind,xn] <- paste("<b>",x[null.ind,xn],"</b>",sep="")
 }
 
 xt$mse[xt$mse>1e15] <- Inf
