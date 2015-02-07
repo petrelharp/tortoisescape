b8e636d0560a6c595073c876c291143b78fc2094
peter <petrel.harp@gmail.com>
2015-02-06 22:33:09 -0800

    .

--- visualization/ibd-slope.R
+++ visualization/ibd-slope.R
@@ -7,8 +7,13 @@ assign("sample.locs",get(sample.loc.obj))
 dem <- raster("../visualization/dem_30.gri")
 
 pcs <- read.csv("pcs.csv")
-geog <- read.csv("geog_distance.csv")
+
 pi <- read.csv("all_angsd_snps.pwp.csv")
+# from Evan 1/31/15
+pi$pi <- pi$pi * ( 56575857 / 1898838430 ) / 2.01e-9
+
+geog <- read.csv("geog_distance.csv")
+geog$distance <- geog$distance / 1000 # now in km
 
 x <- merge( geog, pi, by=c("etort1","etort2") )
 x$dpc <- abs(pcs$PC1[match(x$etort1,pcs$etort)] - pcs$PC1[match(x$etort2,pcs$etort)])
@@ -16,33 +21,57 @@ x <- subset(x,etort1!=etort2 & (! etort1%in% c("etort-296","etort-297") ) & (! e
 
 coords <- coordinates(sample.locs)
 pops <- list(
-        west_mojave = sample.locs[ 
+        west = sample.locs[ 
             ( coords[,1] < -18e5 ) & 
             ( coords[,2] > -3e5 ) & 
             ( coords[,2] < -1.5e5 ) 
         ],
-        south_mojave = sample.locs[
+        south = sample.locs[
             ( coords[,2] < -3e5 ) &
             ( coords[,1]+coords[,2] < -21e5 ) 
         ],
-        east_mojave = sample.locs[
+        east = sample.locs[
             ( coords[,1]+coords[,2] > -21e5 ) &
             ( coords[,1]-coords[,2] > -15.1e5 ) &
             ( pcs$PC1 < 0 )  # ( coords[,2] < -2e5 )
+        ],
+        north_east = sample.locs[
+            ( coords[,2]+coords[,1]/5 > -4.7e5 )
         ]
     )
-pops$north_mojave <- sample.locs[ setdiff( row.names(sample.locs), unlist(lapply(pops,row.names)) ) ]
+pops$north <- sample.locs[ setdiff( row.names(sample.locs), unlist(lapply(pops,row.names)) ) ]
 
 pop.df <- data.frame( 
         etort=unlist(lapply(pops,row.names)),
         group=names(pops)[rep(seq_along(pops),unlist(lapply(pops,length)))],
     stringsAsFactors=FALSE )
 
-x$group1 <- pop.df$group[match(pop.df$etort,x$etort1)]
-x$group2 <- pop.df$group[match(pop.df$etort,x$etort2)]
+x$group1 <- pop.df$group[match(x$etort1,pop.df$etort)]
+x$group2 <- pop.df$group[match(x$etort2,pop.df$etort)]
 
+png(file="ibd-by-pops.png",width=10*288,height=10*288,pointsize=10,res=288)
+#pdf(file="ibd-by-pops.pdf",width=10,height=10,pointsize=10)
+layout(matrix(1:16,nrow=4))
+par(mar=c(3,3,3,1)+.1,mgp=c(2.2,1,0))
 plot(dem)
-for (k in seq_along(pops)) {
-    points(pops[[k]],pch=20,col=k,cex=2)
+for (k in seq_along(pops)) { points(pops[[k]],pch=20,col=k,cex=2) }
+# legend("topleft",pch=20,cex=2,legend=names(pops),col=seq_along(pops))
+# plot( pi ~ distance, data=x, xlab='distance (km)', ylab='divergence', main='all',
+#    pch=20, cex=0.25, ylim=range(x$pi), xlim=c(0,max(x$distance)) )
+for (j in 1:length(pops)) {
+    for (k in j:length(pops)) {
+        usethese <- with(x, ( ( group1==names(pops)[j] ) & ( group2==names(pops)[k] ) ) 
+            | ( ( group1==names(pops)[k] ) & ( group2==names(pops)[j] ) ) )
+        plot( pi ~ distance, data=x, xlab='distance (km)', ylab='divergence', 
+            col=adjustcolor("slategrey",.1),
+            main=paste(names(pops)[c(j,k)],collapse='-'), 
+            pch=20, cex=0.25, ylim=range(x$pi), xlim=c(0,max(x$distance)) )
+        points( pi ~ distance, data=subset(x,usethese), xlab='distance (km)', ylab='divergence', 
+            pch=20, cex=0.25 )
+        points( 3e2+c(-.2e2,0.2e2), c(2.2e6,2.2e6), pch=20, cex=4, col=c(j,k) )
+        this.lm <- lm( pi ~ distance, data=subset(x,usethese) )
+        abline( coef(this.lm) )
+        text( 3e2, 2.3e6, labels=sprintf("y = %2.0f x + %2.0f", coef(this.lm)[2], coef(this.lm)[1]) )
+    }
 }
-legend("topleft",pch=20,cex=2,legend=names(pops),col=seq_along(pops))
+dev.off()
