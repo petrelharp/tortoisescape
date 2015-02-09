ae481acc59745ab3e44d3e6ef659e6837a60f1f0
peter <petrel.harp@gmail.com>
2015-02-08 20:39:16 -0800

    .

--- visualization/ibd-slope.R
+++ visualization/ibd-slope.R
@@ -53,7 +53,7 @@ png(file="ibd-by-pops.png",width=10*288,height=10*288,pointsize=10,res=288)
 #pdf(file="ibd-by-pops.pdf",width=10,height=10,pointsize=10)
 layout(matrix(1:16,nrow=4))
 par(mar=c(3,3,3,1)+.1,mgp=c(2.2,1,0))
-plot(dem)
+plot(dem,legend=FALSE,xaxt='n',yaxt='n')
 for (k in seq_along(pops)) { points(pops[[k]],pch=20,col=k,cex=2) }
 # legend("topleft",pch=20,cex=2,legend=names(pops),col=seq_along(pops))
 # plot( pi ~ distance, data=x, xlab='distance (km)', ylab='divergence', main='all',
@@ -74,4 +74,40 @@ for (j in 1:length(pops)) {
         text( 3e2, 2.3e6, labels=sprintf("y = %2.0f x + %2.0f", coef(this.lm)[2], coef(this.lm)[1]) )
     }
 }
+dev.off()
+
+
+
+png(file="ibd-by-some-pops.png",width=6.5*288,height=2.4*288,pointsize=10,res=288)
+layout(t(1:3))
+par(mar=c(3,3,3,1)+.1,mgp=c(2.2,1,0))
+plot(dem,legend=FALSE,xaxt='n',yaxt='n')
+south.pops <- c("west","south","east")
+south.col <- "purple"
+north.col <- "blue"
+ns.cols <- c(north.col,south.col)[1+(names(pops) %in% south.pops)]
+for (k in seq_along(pops)) { points(pops[[k]],pch=20,col=ns.cols[k],cex=2) }
+both.south <- with(x, ( ( group1%in%south.pops ) & ( group2%in%south.pops ) ) )
+both.north <- with(x, ( (! group1%in%south.pops ) & (! group2%in%south.pops ) ) )
+# within
+    plot( pi ~ distance, data=x, xlab='distance (km)', ylab='mean divergence (years)', 
+        col=adjustcolor("slategrey",.1),
+        main="within groups",
+        pch=20, cex=0.25, ylim=range(x$pi), xlim=c(0,max(x$distance)) )
+    points( pi ~ distance, data=subset(x,both.north), xlab='distance (km)', ylab='divergence', pch=20, cex=0.25, col=adjustcolor(north.col,.25) )
+    points( pi ~ distance, data=subset(x,both.south), xlab='distance (km)', ylab='divergence', pch=20, cex=0.25, col=adjustcolor(south.col,.25) )
+    this.lm <- lm( pi ~ distance, data=subset(x,both.south|both.north) )
+    abline( coef(this.lm), col='black' )
+    # south.lm <- lm( pi ~ distance, data=subset(x,both.south) )
+    # abline( coef(south.lm), col=south.col )
+    # north.lm <- lm( pi ~ distance, data=subset(x,both.north) )
+    # abline( coef(north.lm), col=north.col )
+# between
+    plot( pi ~ distance, data=x, xlab='distance (km)', ylab='mean divergence (years)', 
+        col=adjustcolor("slategrey",.1),
+        main="between groups",
+        pch=20, cex=0.25, ylim=range(x$pi), xlim=c(0,max(x$distance)) )
+    points( pi ~ distance, data=subset(x,!(both.south|both.north)), xlab='distance (km)', ylab='divergence', pch=20, cex=0.25, col="black" )
+    between.lm <- lm( pi ~ distance, data=subset(x,!(both.south|both.north)) )
+    abline( coef(between.lm), col='black' )
 dev.off()
