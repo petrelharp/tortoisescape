cd0eaa46eaf9a2d2ba0d53dec59914eb2ba3a8ae
peter <petrel.harp@gmail.com>
2015-02-11 09:58:16 -0800

    report

--- drecp/report-figs.R
+++ drecp/report-figs.R
@@ -7,6 +7,10 @@ dem <- raster("../geolayers/nussear/habitat-model/dem_30.grd")
 nus <- raster("../geolayers/nussear/habitat-model/nussear.grd")
 crew <- raster("../geolayers/nussear/habitat-model/crew_NA_1km.gri")
 bb <- extent( 3.4e5, 8.7e5, 3.62e6, 4.17e6 )
+county.lines.obj <- load("../visualization/county_lines.Robj") 
+assign("county.lines",spTransform(get(county.lines.obj),  CRSobj=CRS(proj4string(nus)) ) )
+
+nalayer <- raster("../geolayers/nussear/habitat-model/mask_crew_dem_2K_sea_habitat.grd")
 
 tort.loc.obj <- load("../tort_272_info/geog_coords.RData")
 assign( "sample.locs", spTransform( get(tort.loc.obj), CRSobj=CRS(proj4string(nus)) ) )
@@ -18,18 +22,9 @@ png(width=3*288, height=3*288, res=288, file="reference-points.png", pointsize=1
 #pdf(width=2.5, height=2.5, file="reference-points.pdf", pointsize=10)
 par(mar=c(1,1,3,1)+.1)
 plot( crop(mask(nus,crew),bb), legend=FALSE,xaxt='n',yaxt='n',main='reference points')
+lines(county.lines,col=adjustcolor("slategrey",0.5))
 points(ref.points[-isolated.refs], pch=20, cex=0.25)
 dev.off()
-
-require(knitr)
-chunks <- read.csv("chunk-summaries.csv",header=TRUE)
-x <- chunks[order(chunks$mean,decreasing=TRUE),c("chunk","area","mean","max.incr")]
-x$mean <- round(x$mean)
-x$max.incr <- round(x$max.incr)
-colnames(x) <- c("chunk","isolated area","mean isolation","maximum isolation")
-x <- x[c(match("all",x[,1]),setdiff(1:nrow(x),match("all",x[,1]))),]
-
-kable( x, row.names=FALSE )
 
 
 ####
@@ -50,7 +45,6 @@ get.nearby <- function (sub.hts,thresh=.4) {
     return(nearby)
 }
 
-nalayer <- raster("../geolayers/nussear/habitat-model/mask_crew_dem_2K_sea_habitat.grd")
 if (!file.exists("ref_point_area.csv")) {
     dpts <- apply( sapply( 1:length(ref.points), function (k) { values( distanceFromPoints(nalayer,ref.points[k]) ) } ), 1, which.min )
     point.area <- tapply( values(nalayer), dpts, sum, na.rm=TRUE )
@@ -62,7 +56,7 @@ if (!file.exists("ref_point_area.csv")) {
 
 ### table
 
-isolation.thresh <- 15000
+isolation.thresh <- 1500
 nearby.thresh <- 0.4
 total.area <- sum(!is.na(values(nalayer)))
 alt.stats <- sapply(alt.envs, function (x) { with(x, {
@@ -76,43 +70,18 @@ alt.stats <- sapply(alt.envs, function (x) { with(x, {
                 "significantly isolated area (km2)" = sum( point.area[nearby.means>isolation.thresh] ),
                 "percent significantly isolated area" = sum( point.area[nearby.means>isolation.thresh] )/total.area,
                 "mean increase on significantly isolated area" = mean(nearby.means[nearby.means>isolation.thresh]),
+                "mean relative increase on significantly isolated area" = mean( ( rowSums( diffmat*nearby/sym(1+sub.hts) )/rowSums(nearby) )[nearby.means>isolation.thresh]),
                 "isolated area (km2)" = sum( point.area[nearby.means>0] ),
                 "percent isolated area" = sum( point.area[nearby.means>0] )/total.area,
                 "mean increase on isolated area" = mean(nearby.means[nearby.means>0]),
                 "max increase" = max( diffmat[nearby], na.rm=TRUE ),
                 "max decrease" = min( diffmat[nearby], na.rm=TRUE ),
                 "mean relative increase" = mean( ( rowSums( diffmat*nearby/sym(1+sub.hts) )/rowSums(nearby) )[nearby.means>0]),
-                "mean relative increase on isolated area" = mean( ( rowSums( diffmat*nearby/sym(1+sub.hts) )/rowSums(nearby) )[nearby.means>isolation.thresh]),
+                "mean relative increase on isolated area" = mean( ( rowSums( diffmat*nearby/sym(1+sub.hts) )/rowSums(nearby) )[nearby.means>0]),
                 check.names=FALSE)
         } ) 
     } )
 write.csv(alt.stats,file="alternative-summaries.csv")
-
-alt.stats <- read.csv("alternative-summaries.csv",header=TRUE,row.names=1)
-
-alts <- data.frame(
-        "habitat removed (km2)"= floor(unlist(alt.stats["habitat area removed (km2)",])),
-        "(%)"=sprintf("(%0.1f%%)",
-                (100*unlist(alt.stats["percent habitat removed",]))
-            ),
-        "isolated (km2)"= floor(unlist(alt.stats["isolated area (km2)",])),
-        "(%)"=sprintf("(%0.1f%%)",
-                (100*unlist(alt.stats["percent isolated area",]))
-            ),
-        "isolation (years)"= floor(unlist(alt.stats["mean increase on isolated area",])),
-        "strongly isolated (km2)"= floor(unlist(alt.stats["significantly isolated area (km2)",])),
-        "(%)"=sprintf("(%0.1f%%)",
-                (100*unlist(alt.stats["percent significantly isolated area",]))
-            ),
-        "strong isolation (years)"= floor(unlist(alt.stats["mean increase on isolated area",])),
-        "relative isolation "=sprintf("%i%%",
-                floor(100*unlist(alt.stats["mean relative increase on isolated area",]))
-            ),
-        check.names=FALSE
-    )
-rownames(alts) <- c("preferred",paste("alternative",1:4))
-
-kable(alts)
 
 
 ### figures
@@ -156,12 +125,48 @@ for (k in seq_along(alt.names)) {
             x <- tdiffs[,k]
             plot( interp.values(x[alt.good.refs],alt.good.refs), main="mean difference nearby", col=diff.cols, zlim=zlims, 
                 legend.width=2, xaxt='n', yaxt='n', legend.mar=8.1 )
+            lines(county.lines,col=adjustcolor("slategrey",0.5))
             plot( alt, col="slategrey", add=TRUE, legend=FALSE )
             mtext( alt.full.names[k], side=3, line=-1 )
             y <- reldiffs[,k]
             plot( interp.values(y[alt.good.refs],alt.good.refs), main="relative difference nearby", col=diff.cols, zlim=ratio.zlims, 
                 legend.width=2, xaxt='n', yaxt='n', legend.mar=5.1 )
+            lines(county.lines,col=adjustcolor("slategrey",0.5))
             plot( alt, col="lightslategrey", add=TRUE, legend=FALSE )
         } )
     dev.off()
+}
+
+
+###
+# different chunks in the preferred
+
+for (this.alt in names(alt.names)) {
+    load(file.path("../inference/habitat-only/nus_gt_three",paste(this.alt,"_pda_habitat-only_subchunks.RData",sep='')))  # provides chunk.info.list
+    names(chunk.info.list) <- sapply( chunk.info.list, "[[", "this.chunk" )
+
+    chunk.stats <- sapply(chunk.info.list, function (x) { with(list2env(x,parent=alt.envs[[this.alt]]), {
+                diffmat <- sym(sub.alt.hts-sub.hts)
+                nearby <- get.nearby( sub.hts, nearby.thresh )
+                blocked <- attr(sub.alt.hts,"blocked")
+                nearby.means <- rowSums( diffmat*nearby ) / rowSums(nearby)
+                data.frame(
+                    "habitat area removed (km2)" = sum( !is.na( values(nalayer)[ unique(c(blocked,which(values(alt)%in%this.chunk))) ] ) ),
+                    "percent habitat removed" = sum( !is.na( values(nalayer)[ unique(c(blocked,which(values(alt)%in%this.chunk))) ] ) )/total.area,
+                    "significantly isolated area (km2)" = sum( point.area[nearby.means>isolation.thresh] ),
+                    "percent significantly isolated area" = sum( point.area[nearby.means>isolation.thresh] )/total.area,
+                    "mean increase on significantly isolated area" = mean(nearby.means[nearby.means>isolation.thresh]),
+                    "mean relative increase on significantly isolated area" = mean( ( rowSums( diffmat*nearby/sym(1+sub.hts) )/rowSums(nearby) )[nearby.means>isolation.thresh]),
+                    "isolated area (km2)" = sum( point.area[nearby.means>0] ),
+                    "percent isolated area" = sum( point.area[nearby.means>0] )/total.area,
+                    "mean increase on isolated area" = mean(nearby.means[nearby.means>0]),
+                    "max increase" = max( diffmat[nearby], na.rm=TRUE ),
+                    "max decrease" = min( diffmat[nearby], na.rm=TRUE ),
+                    "mean relative increase" = mean( ( rowSums( diffmat*nearby/sym(1+sub.hts) )/rowSums(nearby) )[nearby.means>0]),
+                    "mean relative increase on isolated area" = mean( ( rowSums( diffmat*nearby/sym(1+sub.hts) )/rowSums(nearby) )[nearby.means>0]),
+                    check.names=FALSE)
+            } ) 
+        } )
+
+    write.csv(chunk.stats,file=paste(this.alt,"-chunk-summaries.csv",sep=''))
 }
