87b6ea2dbcf524ecb04416b1e328d570b876dc49
peter <petrel.harp@gmail.com>
2015-06-19 11:20:39 -0700

    minor tweak

--- geolayers/make-spatial-basis.R
+++ geolayers/make-spatial-basis.R
@@ -1,5 +1,6 @@
 require(raster) 
 require(Matrix)
+require(sp)
 
 base.dir <- gsub("tortoisescape/.*","tortoisescape",getwd())
 
@@ -8,7 +9,7 @@ source(file.path(base.dir,"inference","input-output-fns.R"))
 
 nalayer <- raster("nussear/habitat-model/mask_crew_dem_2K_sea_habitat")
 nonmissing <- which(!is.na(values(nalayer)))
-ph <- plot.ht.fn(layer.prefix="nussear/habitat-model/",nonmissing=nonmissing,
+ph <- plot.ht.fn(layer.prefix="nussear/habitat-model/",nonmissing=nonmissing)
 
 ref.pt.file <- file.path(base.dir,"geolayers/reference_points/all_ref_points.RData")
 ref.pt.obj  <- load(ref.pt.file)
@@ -20,4 +21,26 @@ ref.dists <- do.call( cbind, lapply( seq_along(ref.points), function (k) {
 
 
 ph( ref.dists[,1] )
+
+
+#######
+# basis for local population size?
+
+# choose something for the truth to check we can get close
+popsize <- 1e4 * (0.05 + 2/(1+exp(-ref.dists[,1]*10/max(ref.dists))))
+ph(popsize)
+
+# subset
+rpts <- ref.points[sample.int(length(ref.points),20)]
+# pairwise distances
+rpt.dists <- spDists(rpts)
+
+midpoints <- vector(choose(length(rpts),2),mode="list")
+n <- 1
+for (k in seq_along(rpts)[-1]) {
+    for (j in 1:(k-1)) {
+        midpoints[[n]] <- (1/2)*( coordinates(rpts[k]) + coordinates(rpts[j]) )
+        n <- n+1
+    }
+}
 
