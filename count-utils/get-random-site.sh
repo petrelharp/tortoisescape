54fc51c1465f1a12e4d4abd51d5c025d53fef8fd
peter <petrel.harp@gmail.com>
2015-09-06 08:59:20 -0700

    .

--- count-utils/get-random-site.sh
+++ count-utils/get-random-site.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 usage="Usage:
-    $0 (maf file) (counts file) (min frequency) (max frequency) [number of sites]
+    $0  (min frequency) (max frequency) [number of sites] [scaffold]
 
-Chooses random sites with MAF between [min] and [max]; outputs info and corresponding counts to stdout.
+Chooses random sites with MAF between [min] and [max]; 
+optionally restricting to 'scaffold' (a regex) and at least a certain number of individuals (nInd);
+outputs info and corresponding counts to stdout.
 
 "
 
@@ -13,16 +15,35 @@ then
     exit 1
 fi
 
-MAFFILE="$1"
-COUNTFILE="$2"
-MINFREQ="$3"
-MAXFREQ="$4"
+MAFFILE="272torts_snp1e6_minmapq20minq30.mafs.gz"
+POSFILE="272torts_snp1e6_minmapq20minq30.pos.gz"
+COUNTFILE="272torts_snp1e6_minmapq20minq30.counts.gz"
+
+## all the info is:
+# > paste <(zcat 272torts_snp1e6_minmapq20minq30.pos.gz) <(zcat 272torts_snp1e6_minmapq20minq30.mafs.gz | cut -f 3-) | nl
+#  1  chr         pos  totDepth  major  minor  knownEM   pK-EM         nInd
+#  2  scaffold_0  191  195       A      G      0.336019  0.000000e+00  132
+#  3  scaffold_0  471  9         C      T      0.237115  1.676451e-07  8
+#  4  scaffold_0  494  348       T      C      0.019132  4.440892e-16  204
+#  5  scaffold_0  518  331       A      G      0.032015  0.000000e+00  193
+
+MINFREQ="$1"
+MAXFREQ="$2"
 NSITES=${5:-1}  # defaults to 1
+SCAFFOLD="${6:-.}"  # defaults to '.'
+NIND="${7:-150}"  # defaults to 150
 
-# 6th column is MAF
-MAFINFO=$(zcat $MAFFILE | nl | awk -v minfreq=$MINFREQ -v maxfreq=$MAXFREQ '$6>minfreq && $6<maxfreq' | shuf -n $NSITES | sort)
+# 7th column is MAF (after nl)
+MAFPAT="\$7>$MINFREQ && \$7<$MAXFREQ"
+# 2nd is scaffold
+if [ $# -ge 6 ]
+then
+    MAFPAT="$MAFPAT && \$2 ~ $SCAFFOLD"
+fi
 
-SITES=$(echo "$MAFINFO" | cut -f 1)  # line numbers added by nl
+MAFINFO=$(paste <(zcat 272torts_snp1e6_minmapq20minq30.pos.gz) <(zcat 272torts_snp1e6_minmapq20minq30.mafs.gz | cut -f 3-) | nl | awk -f <(echo "$MAFPAT") | shuf -n $NSITES | sort)
+
+SITES=$(echo "$MAFINFO" | cut -f 1)  # extract line numbers added by nl (note both files have a header)
 AWKPAT=$(echo $(for x in $SITES; do echo "NR==$x ||"; done) | sed -e 's/||$/;/')
 
 if [ -z "$SITES" ]
