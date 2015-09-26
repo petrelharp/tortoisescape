cf734b675c6d5a1d17ccf844d0bd502ed5b4ffa5
peter <petrel.harp@gmail.com>
2015-09-26 00:44:03 -0700

    .

--- ibd/looking.R
+++ ibd/looking.R
@@ -12,7 +12,8 @@ names(ibd) <- c("id1","hap1","id2","hap2","chrom","start","end","LOD")
 ids <- unique(c(ibd$id1,ibd$id2))
 nids <- as.numeric( gsub("ind","",ids) )
 ids <- ids[order(nids)]
-geog <- read.csv("../tortoisescape/tort_272_info/geog_distance.csv")
+geog <- read.csv("../tort_272_info/geog_distance.csv")
+pcs <- read.csv("../tort_272_info/pcs.csv")
 etorts <- unique(c(levels(geog$etort1),levels(geog$etort2)))
 netorts <- as.numeric(gsub("[^0-9]","",gsub(" .*","",etorts)))
 etorts <- etorts[order(netorts)]
@@ -46,6 +47,7 @@ nibd <- data.frame(
 
 len.breaks <- c(0,exp(seq(log(1000),log(2e4),length.out=10)),1e5)
 len.mids <- len.breaks[-1] - diff(len.breaks)/2
+len.mids[length(len.mids)] <- rev(len.breaks)[2]
 ibd$length.bin <- cut( ibd$end-ibd$start,breaks=len.breaks )
 
 for (k in 1:nlevels(ibd$length.bin)) {
@@ -65,3 +67,77 @@ pdf(file="ibd-by-distance.pdf",width=10,height=8,pointsize=10)
 matplot(x=dist.mids, y=len.dist, type='l', log='y', ylab="mean number of blocks", xlab="geographical distance", col=rainbow(length(len.mids)), lty=1 )
 legend("topright",lty=1,col=rainbow(length(len.mids)), legend=paste(levels(ibd$length.bin),"bp"))
 dev.off()
+
+## length spectrum by distance
+
+nibd <- data.frame(
+        etort1=etorts[row(geog.mat)[ut]],
+        etort2=etorts[col(geog.mat)[ut]],
+        dist=geog.mat[ut]
+        )
+
+
+len.breaks <- c(0,exp(seq(log(1000),log(2e4),length.out=50)),1e5)
+len.mids <- len.breaks[-1] - diff(len.breaks)/2
+len.mids[length(len.mids)] <- rev(len.breaks)[2]
+ibd$length.bin <- cut( ibd$end-ibd$start,breaks=len.breaks )
+
+for (k in 1:nlevels(ibd$length.bin)) {
+    nibd[[levels(ibd$length.bin)[k]]] <- count.ibds( as.numeric(ibd$length.bin)==k )
+}
+
+dist.breaks <- seq(0,max(geog.mat),length.out=20)
+dist.mids <- dist.breaks[-1]-diff(dist.breaks)/2
+nibd$dist.bin <- cut( nibd$dist, breaks=dist.breaks )
+
+len.dist <- sapply( 1:nlevels(ibd$length.bin), function (k) {
+                tapply( nibd[[levels(ibd$length.bin)[k]]], nibd$dist.bin, mean )
+        } )
+colnames(len.dist) <- levels(ibd$length.bin)
+
+matplot(len.mids, t(len.dist),type='l',log='y')
+
+### by north/south
+
+ns.labels <- ifelse( pcs$PC1[match(etorts,pcs$etort)]>0, "N", "S" )
+nibd$ns <- paste( ns.labels[match(nibd$etort1,etorts)], ns.labels[match(nibd$etort2,etorts)], sep="")
+ns.spectra <- sapply( 1:nlevels(ibd$length.bin), function (k) {
+                tapply( nibd[[levels(ibd$length.bin)[k]]], nibd$ns, mean )
+        } )
+colnames(ns.spectra) <- levels(ibd$length.bin)
+
+matplot(len.mids, t(ns.spectra),type='l',log='y',lty=1:4,col=1:4)
+legend("topright",legend=rownames(ns.spectra),lty=1:4,col=1:4)
+
+####
+
+require(Matrix)
+
+indpair <- function (etort1,etort2) {
+    # return a unique index for an unordered pair of etorts
+    ii <- matrix(1:length(etorts)^2,nrow=length(etorts))
+    ut <- upper.tri(ii,diag=TRUE)
+    ii[ut] <- (1:sum(ut))
+    ii[lower.tri(ii,diag=FALSE)] <- t(ii)[lower.tri(ii,diag=FALSE)]
+    return( ii[ cbind(match(etort1,etorts),match(etort2,etorts)) ] )
+}
+
+chroms <- unique(ibd$chrom)
+etort.pairs <- outer(etorts,etorts,paste,sep="-")
+etort.pairs <- etort.pairs[upper.tri(etort.pairs,diag=TRUE)]
+cii <- with(ibd, sparseMatrix( i=indpair(etort1,etort2), j=match(chrom,chroms), x=len, dims=c(length(etort.pairs),length(chroms)) ) )
+
+contig.means <- colMeans(cii>0)
+pair.means <- rowMeans(cii>0)
+
+# (X-A)^T (X-A) = X^T X - X^T A - A^T X + A^T A
+#  X^T (u 1^T) = X^T u 1^T
+#  X^T (1 v^T) = X^T 1 v^T
+
+contig.cov <- crossprod(cii>0) - outer(colMeans(cii>0),colMeans(cii>0))
+contig.cor <- contig.cov / (nrow(cii)*sqrt( outer(colMeans(cii>0),colMeans(cii>0)) ))
+diag(contig.cor) <- 0
+
+perfects <- which(contig.cor>0.9 & row(contig.cor)>col(contig.cor))
+cbind( row(contig.cor)[perfects], col(contig.cor)[perfects] )
+
